61cda90c9c9a124965835f9846211c531ec8bd2b
VIAO.AI <viao.ai@outlook.com>
2024-09-08 17:41:29 -0400

    config.py

---
+++ color_detection/color_detection/config.py
@@ -0,0 +1,7 @@
+# Definition of colour ranges in HSV for detection
+
+COLOR_RANGES = {
+    "Red": ([0, 100, 100], [10, 255, 255], (0, 0, 255)),
+    "Green": ([35, 40, 40], [75, 255, 255], (0, 255, 0)),
+    "Blue": ([90, 50, 50], [130, 255, 255], (255, 0, 0))
+} 
